0ffc7341a7b260ceda6a0847da4ae1d5d7861b37
kbrannan <brannan.kevin@deq.state.or.us>
2015-12-01 15:53:29 -0800

    added commands to write the flow volumes of storms

--- devel/write-storm-info-to-PEST-files.R
+++ devel/write-storm-info-to-PEST-files.R
@@ -7,12 +7,18 @@ load(file = paste0(getwd(), "/data/storm-data-for-PEST.RData"))
 chr.strm.dates <- paste0(" ",format(df.strm.sum$date.bgn, format = "%m/%d/%Y"), "  00:00:00    ",
        format(df.strm.sum$date.end, format = "%m/%d/%Y"), "  24:00:00")
 
-# storm peaks in cfs
-# use format - "mpeak1_max             7910.000        1.000000E-02  mpeak"
-# mpeak1_max             7910.000        1.000000E-02  mpeak
-# mpeak1_max          7.91000E+03        1.000000E-02  mpeak
+# create vector for storm numbers ti use as ids
+n.strms <- 1:length(df.strm.sum[, 1])
 
-junk <- df.strm.sum[1,]
-junk
-paste0(" mpeak1_max          ", sprintf(junk$peak.tfl, fmt="%1.5E"),
-       "        1.000000E-02  mpeak")
+# storm peaks in cfs
+# use format - "mpeak1_max          7.91000E+03        1.000000E-02  mpeak"
+# create character vector with the storm peaks in cfs
+paste0(" mpeak", sprintf(n.strms, fmt = "%02i"), "_max          ",
+       sprintf(junk$peak.tfl, fmt="%1.5E"), "        1.000000E-02  mpeak")
+
+# storm volumes in cubic feet
+# use format - "mvol_stm_1            2.8778977E+09    1.000000E-02  mvol_stm"
+# create character vector with the storm volumes in cubic feet
+paste0(" mvol_stm_", sprintf(n.strms, fmt = "%02i"), "            ",
+       sprintf(junk$sum.cuft.tfl, fmt="%1.5E"),
+       "        1.000000E-02  mvol_stm")
