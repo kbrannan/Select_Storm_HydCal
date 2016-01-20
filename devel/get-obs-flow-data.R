28279a11c054444a1ed788686db634274d5ce571
kbrannan <brannan.kevin@deq.state.or.us>
2016-01-20 15:08:06 -0800

    made path to data explicit, used new file (and name) and used scan to read file as text

--- devel/get-obs-flow-data.R
+++ devel/get-obs-flow-data.R
@@ -1,16 +1,26 @@
 ## script reads in and adds date (in POSIXct format) observed flow data
 ## for OWRD station 14306030 on the Yaquina River north of Chitwood, OR.
-## I am using publised and provisional mean data daily flow data (in cfs)
-## for the period of 1995-10-01 to 2014-09-30. I downloaded the data 
-## manually from:
-## http://apps.wrd.state.or.us/apps/sw/hydro_near_real_time/display_hydro_graph.aspx?station_nbr=14306030
+## I am using publised mean data daily flow data (in cfs)
+## for the period of 1995-10-01 to 2015-09-30. I downloaded the data 
+## manually in 2016-01-20 from:
+## http://apps.wrd.state.or.us/apps/sw/hydro_report/gage_data_request.aspx?station_nbr=14306030
+## downloaded data as "Formatted Text", which is a fixed format using multiple 
+## spaces to maintain columns
 
 # check environment for existing data to keep
 if(length(ls()) > 0) dont.del <- paste0("(",ls(all.names=TRUE),")",collapse="|")
 
-chr.flow.dir <- paste0(strsplit(getwd(),split="/R_projs",fixed=TRUE)[[1]][1],
-                       "/data")
-chr.flow.file <- "Station_14306030_mean_daily_flow.txt"
+## explcitly set path for location of raw data 
+chr.flow.dir <- "M:/Models/Bacteria/HSPF/HydroCal201506/R_projs/Select_Storm_HydCal/data"
+##chr.flow.dir <- paste0(strsplit(getwd(),split="/R_projs",fixed=TRUE)[[1]][1],
+##                       "/data")
+
+## default file name changed
+##chr.flow.file <- "Station_14306030_mean_daily_flow.txt"
+chr.flow.file <- "station_14306030_mdfDaily_time_series.txt"
+
+chr.obs.data <- scan(file = paste0(chr.flow.dir, "/", chr.flow.file),
+                     what = "caharacter", sep = "\n")
 
 df.flow.obs <- read.table(file=paste0(chr.flow.dir,"/",chr.flow.file), 
                           header = TRUE, sep="\t")
