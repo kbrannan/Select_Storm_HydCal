cad083350a097745c770a19cfbf1a07e9b709921
kbrannan <brannan.kevin@deq.state.or.us>
2016-01-21 14:22:34 -0800

    set path explicity for writing of storm data file

--- devel/write-storm-info-to-PEST-files.R
+++ devel/write-storm-info-to-PEST-files.R
@@ -1,6 +1,9 @@
+## explcitly setting path
+chr.dir <- "M:/Models/Bacteria/HSPF/HydroCal201506/R_projs/Select_Storm_HydCal"
+
 # write storm info for PEST files 
 
-load(file = paste0(getwd(), "/data/storm-data-for-PEST.RData"))
+load(file = paste0(chr.dir, "/data/storm-data-for-PEST.RData"))
 
 # storm dates
 # use format -" 02/05/1996  00:00:00    02/17/1996  24:00:00"
@@ -15,17 +18,17 @@ n.strms <- 1:length(df.strm.sum[, 1])
 # storm peaks in cfs
 # use format - "mpeak1_max          7.91000E+03        1.000000E-02  mpeak"
 # create character vector with the storm peaks in cfs
-chr.stm.peaks <- paste0(" mpeak", sprintf(n.strms, fmt = "%02i"), "_max          ",
-       sprintf(junk$peak.tfl, fmt="%1.5E"), "        1.000000E-02  mpeak")
+chr.stm.peaks <- paste0("mpeak", sprintf(n.strms, fmt = "%02i"), "_max          ",
+       sprintf(df.strm.sum$peak.tfl, fmt="%1.5E"), "        1.000000E-02  mpeak")
 
 # storm volumes in cubic feet
 # use format - "mvol_stm_1            2.8778977E+09    1.000000E-02  mvol_stm"
 # create character vector with the storm volumes in cubic feet
 chr.stm.vols <- paste0("mvol_stm_", sprintf(n.strms, fmt = "%02i"), "            ",
-       sprintf(junk$sum.cuft.tfl, fmt="%1.5E"),
+       sprintf(df.strm.sum$sum.cuft.tfl, fmt="%1.5E"),
        "        1.000000E-02  mvol_stm")
 
 # write storm peaks and vols to file
-cat(cbind(chr.stm.peaks,chr.stm.vols), file = "strm_peaks_vols.dat", sep = "\n")
+cat(cbind(chr.stm.peaks,chr.stm.vols), file = paste0(chr.dir, "/", "strm_peaks_vols.dat"), sep = "\n")
 
 
